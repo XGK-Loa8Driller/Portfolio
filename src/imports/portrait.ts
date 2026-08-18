a3ea4367617865071bf880efe0d674f90220026f
XGK-Loa8Driller <aditya.k31206@gmail.com>
2026-08-18 10:19:30 +0530

    Portrait chunk p12 and assembler

--- src/imports/portrait.ts
+++ src/imports/portrait.ts
@@ -3,9 +3,17 @@ import p1 from './portrait_parts/p1'
 import p2 from './portrait_parts/p2'
 import p3 from './portrait_parts/p3'
 import p4 from './portrait_parts/p4'
+import p5 from './portrait_parts/p5'
+import p6 from './portrait_parts/p6'
+import p7 from './portrait_parts/p7'
+import p8 from './portrait_parts/p8'
+import p9 from './portrait_parts/p9'
+import p10 from './portrait_parts/p10'
+import p11 from './portrait_parts/p11'
+import p12 from './portrait_parts/p12'
 
 // Portrait stored as base64 data-URI chunks so the project stays text-only.
 // Chunks concatenate back into the original data:image/jpeg;base64 source.
-const portrait = p0 + p1 + p2 + p3 + p4
+const portrait = p0 + p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10 + p11 + p12
 
 export default portrait
